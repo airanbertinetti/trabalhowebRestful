56b1a6f48bf14c04063bb7310de4dabff5acd54c
airanbertinetti <airanbertinetti@gmail.com>
2018-07-01 22:36:53 -0300

    versão 0.1.1

--- public/scripts/controllers/InserirTarefasCtrl.js
+++ public/scripts/controllers/InserirTarefasCtrl.js
@@ -4,7 +4,7 @@ angular.module('clientesApp').controller('InserirTarefasCtrl',
     $scope.cliente = clientesService.getCliente($routeParams.id);
         
     $scope.submitForm = function() {
-        $scope.tarefa.clienteID = $scope.cliente.nome
+        $scope.tarefa.clienteID = $scope.cliente._id
         $http.post("/tarefas", $scope.tarefa).
 
         then(function success(response) {
